f3fcf8e1ab4865177990fa8b72b65f19bde915f3
Juantarazq <juan.tarazona@controlonlineinternational.com>
2025-01-16 11:10:44 -0500

    cache

--- CrudEmpleadosClean/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ CrudEmpleadosClean/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1adfbe01d39307ffc85746cbfb55bd9cec363c05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6468e065ff87d5ce06d56e53fc6286f40d77d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
