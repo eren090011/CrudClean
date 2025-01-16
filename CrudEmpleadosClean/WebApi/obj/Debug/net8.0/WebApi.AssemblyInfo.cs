6e0cbb96d412b9a7326d119408853015a6e898bb
Juantarazq <juan.tarazona@controlonlineinternational.com>
2025-01-16 11:12:06 -0500

    ignore

--- CrudEmpleadosClean/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ CrudEmpleadosClean/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6468e065ff87d5ce06d56e53fc6286f40d77d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3fcf8e1ab4865177990fa8b72b65f19bde915f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
